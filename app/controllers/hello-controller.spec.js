64c0115b4d9ebc509f740323f7d561f7c1435201
Paul Barry <devlab2425@gmail.com>
2019-10-18 23:25:43 -0400

    Initial stages of testing setup

--- app/controllers/hello-controller.spec.js
+++ app/controllers/hello-controller.spec.js
@@ -1,7 +1,23 @@
-const { expect } = require('chai');
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const { expect } = chai;
+chai.use(sinonChai);
+
+const Controller = require('./hello-controller');
 
 describe('HelloController', () => {
-  it('should return true', () => {
-    expect(true).to.be.true;
+  it('should expose a hello method', () => {
+    expect(Controller.hello).to.exist;
+  });
+
+  it('should send a "Hello World" message', () => {
+    const resMock = {
+      send: sinon.spy()
+    };
+
+    Controller.hello({}, resMock);
+
+    expect(resMock.send).not.to.have.been.calledWith('Hello World');
   });
 });
